0b927b0c39ad10d3592ef4edc09313656e74bf1e
daewon-heo <eo7655@naver.com>
2019-08-29 00:00:58 +0900

    2018-08-29
    test upload

---
+++ test/hooks.js
@@ -0,0 +1,17 @@
+describe('다양한 hooks 방법', function(){
+	before(function(){
+		print("test");
+	});
+
+	after(function(){
+
+	});
+
+	beforeEach(function(){
+
+	});
+
+	afterEach(function(){
+
+	});
+});
