3dd33f7448768554b3adca6ae3c0e9c11e9e7acd
devdevdev09 <devdevdevdevdev09@gmail.com>
2019-11-02 20:06:29 +0900

    코드 수정

--- example/Js/var_let_const/var_let_const.js
+++ example/Js/var_let_const/var_let_const.js
@@ -7,24 +7,22 @@ for(let i = 0 ; i < 5; i++) {
 console.log(i); // 1
 
 ////////////////////////////////////////
+const PI;           // ERROR
+
 const PI = 3.14;
 
-PI = 3.141592;
+PI = 3.141592;      // ERROR
 
-const PI = 3.141;
+const PI = 3.141;   // ERROR
 
-console.log(PI);
+console.log(PI);    // 3.14
 
-var PI = 3.1415;
-
-let PI = 3.14159;
+var PI = 3.1415;    // ERROR
+let PI = 3.14159;   // ERROR
 
 if(PI == 3.14){
     const PI = 3.141592;
 
-    let PI = 3.1415926;
-
-    console.log(PI);
+    console.log(PI);  // 3.141592
 }
 
-const PI;
