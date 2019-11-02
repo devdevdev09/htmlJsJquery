5099cd7c5327204ee86cc3d59942fbdab7363158
devdevdev09 <devdevdevdevdev09@gmail.com>
2019-11-02 19:55:03 +0900

    es6 let and cost

---
+++ example/Js/var_let_const/var_let_const.js
@@ -0,0 +1,30 @@
+let i = 1;
+
+for(let i = 0 ; i < 5; i++) {
+    console.log(i); // 0 to 4
+}
+
+console.log(i); // 1
+
+////////////////////////////////////////
+const PI = 3.14;
+
+PI = 3.141592;
+
+const PI = 3.141;
+
+console.log(PI);
+
+var PI = 3.1415;
+
+let PI = 3.14159;
+
+if(PI == 3.14){
+    const PI = 3.141592;
+
+    let PI = 3.1415926;
+
+    console.log(PI);
+}
+
+const PI;
