fef6c37bd127a0551124e6cf2d3912366100abd8
devdevdev09 <devdevdevdevdev09@gmail.com>
2019-11-03 22:23:46 +0900

    js hoisting and fn

---
+++ example/Js/fnExpressAndDeclare/fnExpressAndDeclare.js
@@ -0,0 +1,11 @@
+function area(width, heigth){
+    return width*heigth;
+}
+
+console.log(area(5,10));
+
+var area = function(width, height) {
+    return width * height;
+}
+ 
+console.log(area(5,10));
---
+++ example/Js/hoisting/hoisting.js
@@ -0,0 +1,13 @@
+// hoisting1-1
+hoisting();
+
+function hoisting(){
+    alert("hoisting!");
+}
+
+// hoisting1-2
+function hoisting(){
+    alert("hoisting!");
+}
+
+hoisting();
