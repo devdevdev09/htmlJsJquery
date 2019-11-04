fff1c5ef6e0b18ace2301505a4c5da9a1a5980d3
devdevdev09 <devdevdevdevdev09@gmail.com>
2019-11-04 22:00:38 +0900

    jquery remove()

---
+++ example/jQuery/remove/remove.js
@@ -0,0 +1,13 @@
+$(function(){
+    remove();
+});
+
+var remove = function removeElement(){
+    // $("#main").remove();  // div#main 없애기
+
+    // $("button").remove(); // 모든 버튼 없애기
+
+    // $("button").first().remove() // 첫번째 버튼만 없애기
+
+    // $("div").remove("#main");    // div#main 없애기
+}
