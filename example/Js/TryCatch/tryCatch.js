3368f76dab49a2564ef41f0fcff5e7bac29586d1
dev09 <devdevdevdevdev09@gmail.com>
2019-11-11 22:10:55 +0900

    try catch update

--- example/Js/TryCatch/tryCatch.js
+++ example/Js/TryCatch/tryCatch.js
@@ -1,13 +1,36 @@
+// try catch
 try{
-    throw new Error("new message");
+    console.log("try");
+    num[-1] = 0;
 }catch(e){
-    console.log(e.message);
+    console.log(e);
 }
 
-////////////////////////
+// try catch finally
+try{
+    console.log("try");
+    num[-1] = 0;
+}catch(e){
+    console.log(e);
+}finally{
+    console.log("finally message");
+}
 
+// try catch throw
 try{
     throw ("new message");
 }catch(e){
     console.log(e);
 }
+
+// try catch new Error
+try{
+    throw new Error("new message");
+}catch(e){
+    console.log(e.message);
+}finally{
+    console.log("finally message");
+}
+
+////////////////////////
+
